389dc42b0071082f0ff447db7be8062d76d70049
lolychee <lolychee@gmail.com>
2021-07-02 10:29:29 +0000

    update: 更新模板版本

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -8,10 +8,12 @@ const darkBlue = `#007acc`
 // const lightBlue = `#66E0FF`
 const blueGray = `#282c35`
 
-export default {
+const theme = {
     colors: {
         text: blueGray,
         primary: darkBlue,
         heading: blueGray,
     },
 }
+
+export default theme
